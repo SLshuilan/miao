10d1c4dffff09e2240ea7c33d730a81c1c0aa15d
shuilan <1982517954@qq.com>
2022-09-14 20:54:22 +0800

    da

---
+++ SLshuilan-lodash.js
@@ -0,0 +1,20 @@
+var kuai = {
+  chunk: function (array, size = 1) {
+    var a = []
+    var b = []
+    var j = 0
+    for (i = 0; i < array.length; i++) {
+      b.push(array[i])
+      j++
+      if (j >= size) {
+        j = 0
+        a.push(b)
+        b = []
+      }
+    }
+    if (j != 0) {
+      a.push(b)
+    }
+    return a
+  }
+}
